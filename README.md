3e378fcad6f2c39a050ee18d241423f7f1f189ff
quinceyliu <quinceyliu@tencent.com>
2020-09-09 11:28:13 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SparkLocalTest
+
+This is a spark demo I wrote for [a stackoverflow question](https://stackoverflow.com/questions/63683466/java-mapreduce-by-2-fields-into-time-slides).
+Clone the repo and run in any IDE that supports maven extension, it'll be helpful.
--- src/main/java/com/shp/demo/WordCount.java
+++ src/main/java/com/shp/demo/WordCount.java
@@ -13,7 +13,8 @@ public class WordCount {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("word count").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        List<String> data = Arrays.asList("Google Bye GoodBye Hadoop code", "Java code Bye");
+        List<String> data = Arrays.asList("how to sing abc song", "to be or not to be", "that is a question",
+                "sing a song with drake");
         sc.parallelize(data)
                 .flatMap((FlatMapFunction<String, String>) s -> Arrays.asList(s.split(" ")).iterator())
                 .mapToPair((PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1))
